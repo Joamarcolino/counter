dbece2ae19648dac2153d0f07c0c7dd9de3de97e
João Vitor Marcolino Veiga <166542665+Joamarcolino@users.noreply.github.com>
2025-04-23 15:29:49 -0300

    Update 2 ( added a limiter to the negatives )
    - Doesnt go to negative numbers

--- logic.js
+++ logic.js
@@ -4,12 +4,20 @@
      //state changer
      function add(value){
          number = number + value
+         limiter()
          render()
      }
 
+     // renders state and shows it in the page
       function render(){
          const p = document.querySelector("p")
          p.innerText = number
+      }
+
+      function limiter(){
+        if (number <= 0){
+            number = 0
+        }
       }
       //start it
       render()
